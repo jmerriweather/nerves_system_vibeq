4cddf411b19fe913c87e0ee18ee8abdeee81a959
Jonathan Merriweather <Jono@live.com>
2019-03-18 10:59:15 +1100

    remove artifact sites

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule NervesSystemBbb.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_sites: [
-        {:github_releases, "jmerriweather/#{@app}"}
-      ],
+      # artifact_sites: [
+      #   {:github_releases, "jmerriweather/#{@app}"}
+      # ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
